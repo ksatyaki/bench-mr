87a1d3d6f6cfbd5ca77b75f919f82ead8dd3f9f7
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2022-03-20 23:00:19 +0100

    Added and tested MRPT grid-map from ros-yaml

--- src/base/environments/MRPTGridMap.h
+++ src/base/environments/MRPTGridMap.h
@@ -6,10 +6,29 @@
 
 #include <base/Environment.h>
 #include <yaml-cpp/yaml.h>
+#include <mrpt/maps/COccupancyGridMap2D.h>
+#include <memory>
 
 class MRPTGridMap : Environment {
+  std::shared_ptr<mrpt::maps::COccupancyGridMap2D> mrpt_map_;
+
+  float _thresdhold{0.0};
  public:
   MRPTGridMap() = default;
   MRPTGridMap(const std::string& yaml_file_name);
+
+  virtual bool collides(double x, double y) override;
+  virtual bool collides(const Polygon &polygon) override;
+  virtual double distance(double x, double y) override;
+  virtual std::string name() const override { return "MRPT Grid-map"; }
+
+  virtual void to_json(nlohmann::json &j) override {
+    j["type"] = "mrpt";
+    j["width"] = width();
+    j["height"] = height();
+    j["start"] = start();
+    j["goal"] = goal();
+    j["name"] = name();
+  }
 };
 
--- src/base/environments/src/MRPTGridMap.cpp
+++ src/base/environments/src/MRPTGridMap.cpp
@@ -6,8 +6,39 @@
 MRPTGridMap::MRPTGridMap(const std::string& yaml_file_name) {
   YAML::Node map_yaml_node = YAML::LoadFile(yaml_file_name);
 
+  this->mrpt_map_ = std::make_shared<mrpt::maps::COccupancyGridMap2D>();
+  auto folder = yaml_file_name.substr(0, yaml_file_name.find_last_of("/"));
+  if (this->mrpt_map_->loadFromBitmapFile(
+          folder + "/" + map_yaml_node["image"].as<std::string>(),
+          map_yaml_node["resolution"].as<float>(),
+          mrpt::math::TPoint2D(map_yaml_node["origin"][0].as<float>(),
+                               map_yaml_node["origin"][1].as<float>()))) {
+    printf("Successfully loaded an MRPT Gridmap from ros-style YAML.\n");
+  }
+  this->_bounds.setLow(0, mrpt_map_->getXMin());
+  this->_bounds.setHigh(0, mrpt_map_->getXMax());
 
-  std::cout << map_yaml_node["image"].as<std::string>() << "\n"
-            << "Origin x: " << map_yaml_node["origin"][0] << "\n"
-            << "Origin y: " << map_yaml_node["origin"][1];
+  this->_bounds.setLow(1, mrpt_map_->getYMin());
+  this->_bounds.setHigh(1, mrpt_map_->getYMax());
+
+  this->_thresdhold = map_yaml_node["occupied_thresh"].as<double>();
+}
+
+double MRPTGridMap::distance(double x, double y) {
+  return mrpt_map_->computeClearance(x, y, 10.0);
+}
+
+bool MRPTGridMap::collides(double x, double y) {
+  return mrpt_map_->getPos(x, y) > this->_thresdhold;
+}
+
+bool MRPTGridMap::collides(const Polygon& polygon) {
+  bool collision = false;
+  for (auto point : polygon.points) {
+    if (collides(point.x, point.y)) {
+      collision = true;
+      break;
+    }
+  }
+  return collision;
 }
